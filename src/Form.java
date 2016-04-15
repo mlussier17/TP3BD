849df381c59219bb03a492ca8810e2336ff13746
mlussier17 <math.lussier@hotmail.com>
2016-04-15 19:18:01 -0400

    Added buttons on form

--- src/Form.java
+++ src/Form.java
@@ -10,9 +10,20 @@ public class Form {
     private JPanel PAN_Overall;
     private JPanel PAN_Connexion;
     private JButton BT_Disconnect;
-    private JPanel PAN_Controls;
-    private JButton BT_List;
-    private JButton BT_Add;
+    private JPanel PAN_Circuit;
+    private JButton BT_CModify;
+    private JButton BT_CAdd;
+    private JButton BT_CDelete;
+    private JPanel PAN_Client;
+    private JButton BT_CLAdd;
+    private JButton BT_CLDelete;
+    private JButton BT_RAdd;
+    private JButton BT_RDelete;
+    private JButton BT_CList;
+    private JTextField TA_CName;
+    private JPanel PAN_Reservation;
+    private JPanel PAN_List;
+    private JButton BT_MList;
 
     public static void main(String[] args) {
         JFrame frame = new JFrame("Form");
@@ -23,16 +34,43 @@ public class Form {
     }
 
     public Form() {
+        PAN_Circuit.setVisible(false);
+        PAN_Client.setVisible(false);
+        PAN_Reservation.setVisible(false);
+        PAN_List.setVisible(false);
+
+        // Connection button
         BT_Connect.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 Connection.connect();
+                PAN_Circuit.setVisible(true);
+                PAN_Client.setVisible(true);
+                PAN_Reservation.setVisible(true);
+                PAN_List.setVisible(true);
             }
         });
+
+        // Disconnection button
         BT_Disconnect.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 Connection.disconnect();
+            }
+        });
+
+        //List button to list the client on a given circuit from (TA_CName)
+        BT_CList.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+            }
+        });
+
+        //List button to list the monuments on a given circuit from (TA_CName)
+        BT_MList.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
             }
         });
     }
